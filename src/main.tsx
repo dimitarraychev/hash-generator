1a63632512b242a64f261824ce374db0e8757448
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2025-12-19 14:04:04 +0000

    added basename

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render( <BrowserRouter>
-
-        <App />
-
-  </BrowserRouter>);
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename="/integration-utilities">
+    <App />
+  </BrowserRouter>
+);
