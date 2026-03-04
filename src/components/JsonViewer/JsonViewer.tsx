349ef687929f5271d185ee0961be05e03b2e6ee1
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2026-03-04 09:28:40 +0000

    added expand/collapse to json view and fixed css

--- src/components/JsonViewer/JsonViewer.tsx
+++ src/components/JsonViewer/JsonViewer.tsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import ReactJson from "react-json-view";
 import "./JsonViewer.css";
+import expandSvg from "../../assets/expand.svg";
+import collapseSvg from "../../assets/collapse.svg";
 
 interface JsonViewerProps {
   jsonData: {};
@@ -7,17 +10,30 @@ interface JsonViewerProps {
 }
 
 const JsonViewer = ({ jsonData, title }: JsonViewerProps) => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
   return (
     <div className="json-view-wrapper">
       <label className="json-title">{title}</label>
       <ReactJson
         src={jsonData}
         theme="summerfruit"
-        style={{ backgroundColor: "var(--sidebar-bg)", fontSize: "16px" }}
+        style={{ backgroundColor: "var(--card-bg)", fontSize: "16px" }}
         enableClipboard={true}
         displayDataTypes={false}
         displayObjectSize={false}
+        collapsed={isCollapsed}
       />
+
+      <div className="json-buttons-wrapper">
+        <img
+          src={isCollapsed ? expandSvg : collapseSvg}
+          alt={isCollapsed ? "Expand" : "Collapse"}
+          onClick={() => setIsCollapsed((prev) => !prev)}
+          className="expand-btn"
+          title={isCollapsed ? "Expand" : "Collapse"}
+        />
+      </div>
     </div>
   );
 };
